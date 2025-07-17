2c31b1fc6988aac2e22898200e3789bee9331464
Davranaff <davranaff@192.168.0.99>
2025-07-17 17:04:19 +0500

    removed sensetive keys and tokens

--- app/core/config.py
+++ app/core/config.py
@@ -8,27 +8,27 @@ class Settings(BaseSettings):
     database_url: str = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:postgres@localhost:5432")
 
     # Telegram Bot
-    telegram_bot_token: str = os.getenv("TELEGRAM_BOT_TOKEN", "7888828778:AAEH35_WGl3GLWjVZReNcECY_y-OyVW6e-w")
-    webhook_base_url: str = os.getenv("WEBHOOK_BASE_URL", "https://34d7464538fe.ngrok-free.app")
+    telegram_bot_token: str = os.getenv("TELEGRAM_BOT_TOKEN", "bot_token")
+    webhook_base_url: str = os.getenv("WEBHOOK_BASE_URL", "localhost:8000")
 
     # OpenAI
-    openai_api_key: str = os.getenv("OPENAI_API_KEY", "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")
+    openai_api_key: str = os.getenv("OPENAI_API_KEY", "openai_api_key")
 
     # Together
-    together_api_key: str = os.getenv("TOGETHER_API_KEY", "tgp_v1_Kre7Ol-Pm2Rz8mKaw_h8HUfXQi4Xko6JaMPypzPgank")
+    together_api_key: str = os.getenv("TOGETHER_API_KEY", "together_api_key")
 
     # Publishers API Keys
-    medium_api_key: Optional[str] = os.getenv("MEDIUM_API_KEY", None)
-    dev_to_api_key: Optional[str] = os.getenv("DEV_TO_API_KEY", None)
+    medium_api_key: Optional[str] = os.getenv("MEDIUM_API_KEY", "medium_api_key")
+    dev_to_api_key: Optional[str] = os.getenv("DEV_TO_API_KEY", "dev_to_api_key")
 
     # Reddit API
-    reddit_client_id: Optional[str] = os.getenv("REDDIT_CLIENT_ID", None)
-    reddit_client_secret: Optional[str] = os.getenv("REDDIT_CLIENT_SECRET", None)
-    reddit_username: Optional[str] = os.getenv("REDDIT_USERNAME", None)
-    reddit_password: Optional[str] = os.getenv("REDDIT_PASSWORD", None)
+    reddit_client_id: Optional[str] = os.getenv("REDDIT_CLIENT_ID", "reddit_client_id")
+    reddit_client_secret: Optional[str] = os.getenv("REDDIT_CLIENT_SECRET", "reddit_client_secret")
+    reddit_username: Optional[str] = os.getenv("REDDIT_USERNAME", "reddit_username")
+    reddit_password: Optional[str] = os.getenv("REDDIT_PASSWORD", "reddit_password")
 
     # FastAPI
-    secret_key: str = os.getenv("SECRET_KEY", "your-secret-key-here")
+    secret_key: str = os.getenv("SECRET_KEY", "secret_key")
     algorithm: str = "HS256"
     access_token_expire_minutes: int = 30
 
